8e74b9f9ebf90f9eb882b71c3c85ac6459d7d09b
diehlj <diehlj@users.noreply.github.com>
2023-05-25 16:39:16 +0200

    Add files via upload

--- exercises/exercise_mha.py
+++ exercises/exercise_mha.py
@@ -33,12 +33,18 @@ def exercise_multihead_attention():
 
     tf.debugging.assert_near( projection_bias, tf.zeros_like(projection_bias) ) # Per default, bias initializer is zero.
     assert query_kernel.shape == (11,5,3)
+    assert key_kernel.shape   == (13,5,3)
+    assert value_kernel.shape == (17,5,3)
+    # assert value_kernel.shape == 
     assert projection_bias.shape == (11,) # 'the query input last dimension if output_shape is None'
-    q = TODO
+    # query ~ (2,7,11), query_kernel ~ (11,5,3) => q ~ (2,7,5,3)
+    q = tf.einsum('????', query_kernel, query)
     assert q.shape == (2, 7, 5, 3)
-    k = TODO
+    # key ~ (2,9,13), key_kernel ~ (13,5,3) => k ~ (2,9,5,3)
+    k = tf.einsum('????', key_kernel, key)
     assert k.shape == (2, 9, 5, 3)
-    v = TODO
+    # value ~ (2,9,17), value_kernel ~ (17,5,3) => v ~ (2,9,5,3)
+    v = tf.einsum('????', value_kernel, value)
     assert v.shape == (2, 9, 5, 3)
     inner = TODO
     inner /= tf.sqrt(3.)
@@ -46,5 +52,28 @@ def exercise_multihead_attention():
     multi_head_output = TODO
     tmp = tf.einsum("...NHK, ...HKO -> ...NO", multi_head_output, projection_kernel)
     tf.debugging.assert_near( tmp, tf_result )
+
+def test_attention():
+    '''Understand the inner workings of tf.keras.layers.Attention.'''
+    inputA = tf.keras.Input(shape=(3,4))
+    inputB = tf.keras.Input(shape=(3,4))
+    inputC = tf.keras.Input(shape=(3,4))
+    ##
+    # key: Optional key Tensor of shape [batch_size, Tv, dim]. If not given, will use value for both key and value, which is the most common case.
+    ##
+    at = tf.keras.layers.Attention()([inputA, inputB, inputC])
+    model = tf.keras.Model(inputs=[inputA,inputB,inputC], outputs=at, name="ATTTT")
+
+    tf.random.set_seed(123)
+    queries = tf.random.uniform( (1,3,4) )
+    x = [1.,100.,100000.]
+    y = [10.,10.,10.,10.]
+    values = tf.tensordot(x,y,axes=0)[None,:,:]
+    print(values, values.shape)
+    keys   = tf.random.uniform( (1,3,4) )
+
+    by_hand = TODO
+
+    tf.debugging.assert_equal( by_hand, model([queries,values,keys]) )
 
 exercise_multihead_attention()
